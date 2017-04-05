a5fb82f58284fb853ab6d978c4ed28056d355187
sottar <ohrst.18@gmail.com>
2017-04-05 13:28:25 +0900

    fix typo, path

--- advanced/src/containers/Root.js
+++ advanced/src/containers/Root.js
@@ -1,5 +1,5 @@
-import React, {Component} from 'react'
-import {Provider} from 'react-redux'
+import React, { Component } from 'react'
+import { Provider } from 'react-redux'
 import configureStore from '../configureStore'
 import AsyncApp from './AsyncApp'
 
@@ -7,11 +7,10 @@ const store = configureStore()
 
 export default class Root extends Component {
   render() {
-    return(
+    return (
       <Provider store={store}>
         <AsyncApp />
       </Provider>
     )
   }
 }
-
